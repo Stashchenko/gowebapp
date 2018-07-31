2a432f88b41ea6f602548616cdda92bbaa739ddf
Volodymyr Stashchenko <stashchenko@fidor.de>
2018-07-31 14:47:47 +0300

    fix default Update/Index action

--- app/models/comment.go
+++ app/models/comment.go
@@ -31,15 +31,15 @@ func AddComment(m Comment) (comment Comment, err error) {
 
 // UpdateComment update a Comment into database and returns
 // last nil on success.
-func (m Comment) UpdateComment() error {
+func (m *Comment) UpdateComment() error {
 	c := newCommentCollection()
 	defer c.Close()
-
-	err := c.Session.Update(bson.M{
+	err := mergeComment(m)
+	err = c.Session.Update(bson.M{
 		"_id": m.ID,
 	}, bson.M{
 		"$set": bson.M{
-			"author": m.Author, "text": m.Text, "updatedAt": time.Now()},
+			"author": m.Author, "text": m.Text, "updated_at": m.UpdatedAt},
 	})
 	return err
 }
@@ -66,6 +66,9 @@ func GetComments(limit int, sort string) ([]Comment, error) {
 	defer c.Close()
 
 	err = c.Session.Find(nil).Sort(sort).Limit(limit).All(&comments)
+	if comments == nil {
+		comments = []Comment{}
+	}
 	return comments, err
 }
 
@@ -82,4 +85,17 @@ func GetComment(id bson.ObjectId) (Comment, error) {
 
 	err = c.Session.Find(bson.M{"_id": id}).One(&comment)
 	return comment, err
+}
+
+func mergeComment(m *Comment) error {
+	originComment, err := GetComment(m.ID)
+	m.UpdatedAt = time.Now()
+	m.CreatedAt = originComment.CreatedAt
+	if m.Author == "" {
+		m.Author = originComment.Author
+	}
+	if m.Text == "" {
+		m.Text = originComment.Text
+	}
+	return err
 }
