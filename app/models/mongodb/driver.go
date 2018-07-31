beedf1013dd06057ff6a9536a9a2ef5835118246
Volodymyr Stashchenko <stashchenko@fidor.de>
2018-07-31 12:10:32 +0300

    add mongo config

--- app/models/mongodb/driver.go
+++ app/models/mongodb/driver.go
@@ -1,5 +1,7 @@
 package mongodb
 
+import "log"
+
 var MaxPool int
 var PATH string
 var DBNAME string
@@ -7,6 +9,7 @@ var DBNAME string
 func CheckAndInitServiceConnection() {
 	if service.baseSession == nil {
 		service.URL = PATH
+		log.Printf("=CheckAndInitServiceConnection: Path = %v", PATH)
 		err := service.New()
 		if err != nil {
 			panic(err)
